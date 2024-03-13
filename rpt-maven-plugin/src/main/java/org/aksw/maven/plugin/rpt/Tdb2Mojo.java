0f7ee43f7778fad512221bfd5ee4ca20d1c7d4e9
Claus Stadler <RavenArkadon@googlemail.com>
2024-03-13 18:44:17 +0100

    tdb2-maven-plugin is now a standalone module which only dependes on jena-tdb2

--- rpt-maven-plugin/src/main/java/org/aksw/maven/plugin/rpt/Tdb2Mojo.java
+++
@@ -1,6 +0,0 @@
-package org.aksw.maven.plugin.rpt;
-
-import org.apache.maven.plugins.annotations.Mojo;
-
-@Mojo(name = "load-tdb2")
-public class Tdb2Mojo extends Tdb2MojoShared {}
--- tdb2-maven-plugin/src/main/java/org/aksw/maven/plugin/jena/Tdb2MojoShared.java
+++ tdb2-maven-plugin/src/main/java/org/aksw/maven/plugin/jena/Tdb2MojoShared.java
@@ -1,4 +1,4 @@
-package org.aksw.maven.plugin.rpt;
+package org.aksw.maven.plugin.jena;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -8,20 +8,20 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Consumer;
 
-import org.aksw.commons.model.maven.domain.api.MavenEntityCore;
-import org.aksw.commons.model.maven.domain.impl.MavenEntityCoreImpl;
-import org.aksw.commons.util.derby.DerbyUtils;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.apache.jena.dboe.base.file.Location;
+import org.apache.jena.graph.Node;
 import org.apache.jena.sparql.core.DatasetGraph;
 import org.apache.jena.sparql.exec.UpdateExec;
 import org.apache.jena.sparql.modify.request.UpdateLoad;
@@ -31,6 +31,8 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
@@ -47,9 +49,8 @@ import org.eclipse.aether.resolution.DependencyResult;
 import org.eclipse.aether.util.artifact.JavaScopes;
 import org.eclipse.aether.util.filter.DependencyFilterUtils;
 
+@Mojo(name = "load", defaultPhase = LifecyclePhase.PACKAGE)
 public class Tdb2MojoShared extends AbstractMojo {
-
-    static { DerbyUtils.disableDerbyLog(); }
 
     /** The repository system (Aether) which does most of the management. */
     @Component
@@ -70,7 +71,16 @@ public class Tdb2MojoShared extends AbstractMojo {
     @Component
     private MavenProjectHelper mavenProjectHelper;
 
-    /** Comma separated list of dependency types which to include */
+    /**
+     * Comma separated list of dependency type suffixes which to include.
+     * A type matches if the string after stripping the suffix is empty or ends with a dot.
+     *
+     * Examples:
+     * "nt" matches the suffix "nt" because "" is the empty string.
+     * "rml.ttl" matches the suffix "ttl" because "rml." ends with a dot.
+     * "hint" does NOT match "nt" because "hi" is neither the empty string nor does it end with a dot.
+     *
+     */
     @Parameter(defaultValue = "nt,ttl,nq,trig,owl,nt.gz,ttl.gz,nq.gz,trig.gz,owl.gz,nt.bz2,ttl.bz2,nq.bz2,trig.bz2,owl.bz2")
     private String includeTypes;
 
@@ -96,8 +106,6 @@ public class Tdb2MojoShared extends AbstractMojo {
 
     public void doExecute() throws Exception {
         Log logger = getLog();
-
-        logger.info("TDB2 loader started");
 
         // Test creation first before resolving dependencies
         Location location = Location.create(outputFolder.toPath());
@@ -143,36 +151,72 @@ public class Tdb2MojoShared extends AbstractMojo {
             File artifactFile = artifactResult.getArtifact().getFile();
             String artifactPath = artifactFile.getAbsolutePath();
 
-            String graphName = "urn:mvn:" + MavenEntityCore.toString(new MavenEntityCoreImpl(artifact.getGroupId(), artifact.getArtifactId(), artifact.getVersion(), artifact.getExtension(), artifact.getClassifier()));
+            String graphName = "urn:mvn:" + toString(artifact);
 
             logger.info("Selecting TDB2 workload: " + artifactPath + " -> " + graphName);
 
             UpdateLoad update = new UpdateLoad(artifactPath, graphName);
             workloads.add(update);
-            // MavenEntity artifact = new MavenEntityCoreImpl(filename, filename, filename, filename, filename);
-            // out artifactResult..toString();
-
-            // Files.copy(artifactFile.toPath(), outputDirectory.toPath().resolve(filename));
         }
 
+        // XXX This is a simple change detection procedure that needs to evolve in the future.
+        // It does not handle the case where multiple files are mapped to the same graph.
+        // In general, we need to create a metadata file, tdb instance or graph to do the book-keeping
+
+        boolean[] change = {false};
         DatasetGraph dg = TDB2Factory.connectDataset(location).asDatasetGraph();
         try {
-            Txn.executeWrite(dg, () -> {
-                for (UpdateLoad update : workloads) {
+            for (UpdateLoad update : workloads) {
+                Node destNode = update.getDest();
+                logger.info("Preparing TDB2 workload: " + update.getSource() + " -> " + update.getDest());
+                boolean isAlreadyLoaded = Txn.calculateRead(dg, () -> dg.containsGraph(destNode));
+                if (isAlreadyLoaded) {
+                    logger.info("Skipping TDB2 workload (already loaded): " + update.getSource() + " -> " + update.getDest());
+                } else {
                     logger.info("Executing TDB2 workload: " + update.getSource() + " -> " + update.getDest());
-                    UpdateExec.dataset(dg).update(update).execute();
+                    Txn.executeWrite(dg, () -> {
+                        UpdateExec.dataset(dg).update(update).execute();
+                        change[0] = true;
+                    });
                 }
-            });
+            }
         } finally {
             dg.close();
         }
 
-        try (OutputStream fOut = Files.newOutputStream(outputFile.toPath());
+        Path outputFolderPath = outputFolder.toPath().toAbsolutePath();
+
+        Path tgtFile = outputFile.toPath().toAbsolutePath();
+        Path tgtTmpFile = tgtFile.resolveSibling("." + tgtFile.getFileName().toString());
+
+        if (Files.exists(tgtFile) && !change[0]) {
+            logger.info("No changes detected. Archive already exists: " + tgtFile);
+        } else {
+            logger.info("Writing temp archive: " + tgtTmpFile);
+            packageTdb2(logger::info, tgtTmpFile, outputFolderPath);
+            atomicMoveOrCopy(logger::warn, tgtTmpFile, tgtFile);
+            logger.info("Created archive: " + tgtFile);
+        }
+
+        mavenProjectHelper.attachArtifact(project, outputFile, "tdb2.tar.gz");
+    }
+
+    public static void atomicMoveOrCopy(Consumer<String> warnCallback, Path source, Path target) throws IOException {
+        try {
+            Files.move(source, target, StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            warnCallback.accept(String.format("Atomic move from %s to %s failed, falling back to copy", source, target));
+            Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+    public static void packageTdb2(Consumer<String> fileCallback, Path fileToWrite, Path folderToPackage) throws IOException {
+        try (OutputStream fOut = Files.newOutputStream(fileToWrite);
             BufferedOutputStream buffOut = new BufferedOutputStream(fOut);
             GzipCompressorOutputStream gzOut = new GzipCompressorOutputStream(buffOut);
             TarArchiveOutputStream tOut = new TarArchiveOutputStream(gzOut)) {
 
-            Files.walkFileTree(outputFolder.toPath(), new FileVisitor<Path>() {
+            Files.walkFileTree(folderToPackage, new FileVisitor<Path>() {
                 @Override
                 public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
                     return FileVisitResult.CONTINUE;
@@ -181,9 +225,11 @@ public class Tdb2MojoShared extends AbstractMojo {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     // Skip lock files
-                    if (!file.getFileName().endsWith(".lock")) {
-                        TarArchiveEntry tarEntry = new TarArchiveEntry(file, file.getFileName().toString());
+                    if (!file.getFileName().toString().endsWith(".lock")) {
+                        Path relPath =  folderToPackage.relativize(file);
+                        TarArchiveEntry tarEntry = new TarArchiveEntry(file, relPath.toString());
                         tOut.putArchiveEntry(tarEntry);
+                        fileCallback.accept("Adding: " + file);
                         Files.copy(file, tOut);
                         tOut.closeArchiveEntry();
                     }
@@ -203,7 +249,17 @@ public class Tdb2MojoShared extends AbstractMojo {
 
             tOut.finish();
         }
+    }
 
-        mavenProjectHelper.attachArtifact(project, outputFile, "tdb2.tar.gz");
+    protected String toString(Artifact coord) {
+        String t = coord.getExtension();
+        String c = coord.getClassifier();
+
+        String suffix =
+                (t == null || t.isEmpty() ? "" : ":" + t) +
+                (c == null || c.isEmpty() ? "" : ":" + c);
+
+        String result = coord.getGroupId() + ":" + coord.getArtifactId() + ":" + coord.getVersion() + suffix;
+        return result;
     }
 }
